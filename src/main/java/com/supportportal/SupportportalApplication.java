8bce63d8f6e9fa5a00222a77b952869317505040
sotikele <sotirios.kelesidis@onelity.com>
2021-02-14 16:20:28 +0200

    init commit

---
+++ src/main/java/com/supportportal/SupportportalApplication.java
@@ -0,0 +1,26 @@
+package com.supportportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.io.File;
+
+import static com.supportportal.constant.FileConstant.USER_FOLDER;
+
+@SpringBootApplication
+public class SupportportalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupportportalApplication.class, args);
+        new File(USER_FOLDER).mkdirs();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
---
+++ src/main/java/com/supportportal/exception/domain/ExceptionHandling.java
@@ -0,0 +1,104 @@
+package com.supportportal.exception.domain;
+
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.supportportal.domain.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.persistence.NoResultException;
+import java.io.IOException;
+
+
+@RestControllerAdvice
+public class ExceptionHandling {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+
+    private static final  String ACCOUNT_LOCKED = "Your account has been locked. Please Contact administation";
+    private static final  String METHOD_IS_NOT_ALLOWED = "This request method is not allowed on this endpoint. Please send a '%s' request";
+    private static final  String INTERNAL_SERVER_ERROR_MSG = "YAn error occurred while processing the request";
+    private static final  String INCORRECT_CREDENTIALS = "Username / password incorrect. Please try again";
+    private static final  String ACCOUNT_DISABLED = "Your account has been disabled.If this is an error, please contact administration";
+    private static final  String ERROR_PROCESSING_FILE = "Error occurred while processing file";
+    private static final  String NOT_ENOUGH_PERMISSION = "You do not have enough permission";
+
+    @ExceptionHandler(DisabledException.class)
+    public ResponseEntity<HttpResponse> accountDisabledException(){
+        return  createHttpResponse(HttpStatus.BAD_REQUEST,ACCOUNT_DISABLED);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<HttpResponse> badCredentialsException(){
+        return  createHttpResponse(HttpStatus.BAD_REQUEST,INCORRECT_CREDENTIALS);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<HttpResponse> accessDeniedException(){
+        return  createHttpResponse(HttpStatus.FORBIDDEN,NOT_ENOUGH_PERMISSION);
+    }
+
+    @ExceptionHandler(LockedException.class)
+    public ResponseEntity<HttpResponse> lockedException(){
+        return  createHttpResponse(HttpStatus.UNAUTHORIZED,ACCOUNT_LOCKED);
+    }
+
+    @ExceptionHandler(TokenExpiredException.class)
+    public ResponseEntity<HttpResponse> tokenExpiredException(TokenExpiredException exception){
+        return  createHttpResponse(HttpStatus.UNAUTHORIZED,exception.getMessage());
+    }
+
+    @ExceptionHandler(EmailExistException.class)
+    public ResponseEntity<HttpResponse> emailExistException(EmailExistException exception){
+        return  createHttpResponse(HttpStatus.BAD_REQUEST,exception.getMessage());
+    }
+
+    @ExceptionHandler(UsernameExistException.class)
+    public ResponseEntity<HttpResponse> usernameExistException(UsernameExistException exception){
+        return  createHttpResponse(HttpStatus.BAD_REQUEST,exception.getMessage());
+    }
+
+    @ExceptionHandler(EmailNotFoundException.class)
+    public ResponseEntity<HttpResponse> emailNotFoundException(EmailNotFoundException exception){
+        return  createHttpResponse(HttpStatus.BAD_REQUEST,exception.getMessage());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<HttpResponse> userNotFoundException(UserNotFoundException exception){
+        return  createHttpResponse(HttpStatus.BAD_REQUEST,exception.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<HttpResponse> internalServerErrorException(Exception exception){
+        LOGGER.error(exception.getMessage());
+        return  createHttpResponse(HttpStatus.INTERNAL_SERVER_ERROR,INTERNAL_SERVER_ERROR_MSG);
+    }
+
+    @ExceptionHandler(NoResultException.class)
+    public ResponseEntity<HttpResponse> notFoundException(NoResultException exception){
+        LOGGER.error(exception.getMessage());
+        return  createHttpResponse(HttpStatus.NOT_FOUND,exception.getMessage());
+    }
+
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<HttpResponse> ioException(IOException exception){
+        LOGGER.error(exception.getMessage());
+        return  createHttpResponse(HttpStatus.INTERNAL_SERVER_ERROR,ERROR_PROCESSING_FILE);
+    }
+
+
+    private ResponseEntity<HttpResponse> createHttpResponse(HttpStatus httpStatus,String message){
+        HttpResponse httpResponse = new HttpResponse(httpStatus.value(),httpStatus,httpStatus.getReasonPhrase().toUpperCase(),message);
+
+        return new ResponseEntity<>(httpResponse,httpStatus);
+
+    }
+
+}
