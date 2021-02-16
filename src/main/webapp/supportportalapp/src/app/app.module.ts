d4e4d43258243f8c5417c448804a856c3a6f0e7d
sotikele <sotirios.kelesidis@onelity.com>
2021-02-16 20:12:02 +0200

    add http   interceptor

--- src/main/webapp/supportportalapp/src/app/app.module.ts
+++ src/main/webapp/supportportalapp/src/app/app.module.ts
@@ -3,7 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {AuthenticationService} from "./service/authentication.service";
+import {UserService} from "./service/user.service";
+import {AuthInterceptor} from "./interceptor/auth.interceptor";
 
 @NgModule({
   declarations: [
@@ -14,7 +17,7 @@ import {HttpClientModule} from "@angular/common/http";
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [AuthenticationService, UserService ,{provide: HTTP_INTERCEPTORS , useClass: AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
