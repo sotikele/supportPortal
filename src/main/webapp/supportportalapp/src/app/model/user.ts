66e4691c082c9ac271c6d5ea60a411e86a08521e
sotikele <sotirios.kelesidis@onelity.com>
2021-02-16 19:17:59 +0200

    add user service

--- src/main/webapp/supportportalapp/src/app/model/user.ts
+++ src/main/webapp/supportportalapp/src/app/model/user.ts
@@ -1,3 +1,27 @@
 export class User{
 
+  public  id: number | undefined;
+  public  userId: string | undefined;
+  public  firstName: string;
+  public  lastName: string;
+  public  username:string;
+  public  email: string;
+  public  profileImageUrl: string | undefined;
+  public  logInDateDisplay: Date | undefined;
+  public  joinDate: Date | undefined;
+  public  role: string;
+  public  authorities: [];
+  public  active: boolean;
+  public  notLocked: boolean;
+
+  constructor() {
+    this.firstName = '';
+    this.lastName  = '';
+    this.username  = '';
+    this.email  = '';
+    this.active = false;
+    this.notLocked = false;
+    this.role = '';
+    this.authorities = [];
+  }
 }
