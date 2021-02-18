d32eba7675fa53e07b10e34acc97e90a50a572d2
sotikele <sotirios.kelesidis@onelity.com>
2021-02-18 17:33:04 +0200

    add notification module and service

--- src/main/webapp/supportportalapp/src/app/guard/authentication.guard.ts
+++ src/main/webapp/supportportalapp/src/app/guard/authentication.guard.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@angular/core';
-import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 import {AuthenticationService} from "../service/authentication.service";
+import {NotificationService} from "../service/notification.service";
+import {NotificationType} from "../enum/notification-type.enum";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationGuard implements CanActivate {
 
-  constructor(private authenticationService: AuthenticationService,private router: Router) {
+  constructor(private authenticationService: AuthenticationService,private router: Router
+  ,private notificationService: NotificationService) {
   }
 
   canActivate(
@@ -20,7 +23,7 @@ export class AuthenticationGuard implements CanActivate {
         return true;
       }
       this.router.navigate(['/login']);
-      //TODO send notification to user
+      this.notificationService.notify(NotificationType.ERROR, 'You need to log in to access this page'.toUpperCase());
     return false;
   }
 
